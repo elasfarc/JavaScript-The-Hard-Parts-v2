34cd916e469f357cd0b081d7718157488ecfc6bf
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-12-29 23:15:42 +0200

    Add #myReduce

--- callbacks.js
+++ callbacks.js
@@ -69,3 +69,25 @@ function mapWith(list, callbackFn) {
   });
   return mappedList;
 }
+
+/*
+Challenge 6
+Create a function called reduce that takes an array and reduces the elements to a single value. For example it can sum all the numbers, multiply them, or any operation that you can put into a function.
+const nums = [4, 1, 3];
+const add = function(a, b) { return a + b; }
+reduce(nums, add, 0);   //-> 8
+Here's how it works. The function has an "accumulator value" which starts as the initialValue and accumulates the output of each loop. The array is iterated over, passing the accumulator and the next array element as arguments to the callback. The callback's return value becomes the new accumulator value. The next loop executes with this new accumulator value. In the example above, the accumulator begins at 0. add(0,4) is called. The accumulator's value is now 4. Then add(4, 1) to make it 5. Finally add(5, 3) brings it to 8, which is returned.
+*/
+
+function myreduce(array, callback, initial) {
+  let memo = initial;
+  for (let i = 0; i < array.length; i++) {
+    if (i === 0 && !memo) memo = array[i];
+    else memo = callback(memo, array[i]);
+  }
+  return memo;
+}
+
+let x = myreduce([1, 7, 3], (acc, ele) => acc + ele); //11
+let y = myreduce([1, 7, 3], (acc, ele) => acc + ele, 10); //21
+console.log(x, y);

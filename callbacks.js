af220c5c0fb823fb97e2ab5889b880709c19d22f
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-12-29 18:48:17 +0200

    Add #mapWith

--- callbacks.js
+++ callbacks.js
@@ -56,3 +56,16 @@ function myForEach(array, callbackFn) {
     callbackFn(element);
   }
 }
+
+/*
+Challenge 5
+In challenge 3, you've created a function called map. In this challenge, you're going to rebuild the map function by creating a function called mapWith. This time you're going to use forEach inside of mapWith instead of using a for loop.
+*/
+
+function mapWith(list, callbackFn) {
+  const mappedList = [];
+  myForEach(list, (element) => {
+    mappedList.push(callbackFn(element));
+  });
+  return mappedList;
+}

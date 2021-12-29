44ec1d7cc440fd0c96bf60f071125cc14d74e33e
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-12-29 18:39:03 +0200

    Add #addTwo & #addS

---
+++ callbacks.js
@@ -0,0 +1,17 @@
+/*
+Challenge 1
+Create a function addTwo that accepts one input and adds 2 to it.
+*/
+
+function addTwo(input) {
+  return input + 2;
+}
+
+/*
+Challenge 2
+Create a function addS that accepts one input and adds an "s" to it.
+*/
+
+function addS(input) {
+  return input + "s";
+}

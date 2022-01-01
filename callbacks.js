6f58b282512f2b03180ffd8333fef88b3182f25e
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-02 01:50:49 +0200

    Add #union

--- callbacks.js
+++ callbacks.js
@@ -118,3 +118,27 @@ function intersection(...arrays) {
 console.log(
   intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])
 ); // should log: [5, 15]
+
+/*
+Challenge 8
+Construct a function union that compares input arrays and returns a new array that contains all elements. If there are duplicate elements, only add it once to the new array. Preserve the order of the elements starting from the first element of the first input array. BONUS: Use reduce!
+*/
+
+function union(...arrays) {
+  if (arrays.length === 0) return arrays;
+  return myreduce(
+    arrays,
+    (acc, arr, i) => {
+      if (i === 0) return arr;
+      const union = acc;
+      arr.forEach((ele) => {
+        if (!acc.some((accEle) => ele === accEle)) union.push(ele);
+      });
+      return union;
+    },
+    []
+  );
+}
+
+console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
+// should log: [5, 10, 15, 88, 1, 7, 100]

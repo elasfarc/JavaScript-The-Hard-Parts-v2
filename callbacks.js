083b2c0b4b2ccec32ee29b94de00e860f451ad74
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-02 04:32:22 +0200

    Add #objectFilter

--- callbacks.js
+++ callbacks.js
@@ -201,3 +201,25 @@ console.log(
   )
 );
 // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+
+/*
+Challenge 11
+Construct a function objectFilter that accepts an object as the first parameter and a callback function as the second parameter.
+ objectFilter will return a new object. The new object will contain only the properties from the input object such that the property's value is equal to the property's key passed into the callback.
+*/
+
+function objectFilter(obj, cb) {
+  const storage = {};
+  Object.entries(obj).forEach((entry) => {
+    if (entry[1] === cb(entry[0])) storage[entry[0]] = entry[1];
+  });
+  return storage;
+}
+
+const cities = {
+  London: "LONDON",
+  LA: "Los Angeles",
+  Paris: "PARIS",
+};
+console.log(objectFilter(cities, (city) => city.toUpperCase()));
+// Should log { London: 'LONDON', Paris: 'PARIS'}

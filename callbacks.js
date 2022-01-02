27e38d00a53387a7c54b5b537615123530b45258
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-02 04:55:36 +0200

    Add #prioritize

--- callbacks.js
+++ callbacks.js
@@ -234,7 +234,6 @@ function majority(arr, cb) {
   arr.forEach((ele) => {
     if (cb(ele)) trues++;
   });
-  console.log(trues);
   return trues > arr.length - trues ? true : false;
 }
 
@@ -243,3 +242,25 @@ const isOdd = function (num) {
 };
 console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
 console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
+
+/*
+Challenge 13
+Create a function prioritize that accepts an array and a callback. The callback will return either true or false. prioritize will iterate through the array and perform the callback on each element, and return a new array, where all the elements that yielded a return value of true come first in the array, and the rest of the elements come second.
+*/
+
+function prioritize(arr, cb) {
+  return union(
+    arr.filter((ele) => cb(ele)),
+    arr
+  );
+}
+const startsWithS = function (str) {
+  return str[0] === "s" || str[0] === "S";
+};
+console.log(
+  prioritize(
+    ["curb", "rickandmorty", "seinfeld", "sunny", "friends"],
+    startsWithS
+  )
+);
+// should log: ['seinfeld', 'sunny', 'curb', 'rickandmorty', 'friends']

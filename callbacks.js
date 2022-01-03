addf8ab749925b8a2f7b2f789a477adfdf3bdde9
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-03 03:31:45 +0200

    Add #goodKeys

--- callbacks.js
+++ callbacks.js
@@ -302,3 +302,23 @@ const floored = function (num) {
   return Math.floor(num);
 };
 console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
+
+/*
+Challenge 16
+Create a function goodKeys that accepts an object and a callback. The callback will return either true or false. goodKeys will iterate through the object and perform the callback on each value. goodKeys will then return an array consisting only the keys whose associated values yielded a true return value from the callback.
+*/
+function goodKeys(obj, callback) {
+  return Object.keys(obj).filter((key) => callback(obj[key]));
+}
+
+const sunny = {
+  mac: "priest",
+  dennis: "calculating",
+  charlie: "birdlaw",
+  dee: "bird",
+  frank: "warthog",
+};
+const startsWithBird = function (str) {
+  return str.slice(0, 4).toLowerCase() === "bird";
+};
+console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']

285bc8414d4b9835e028d7f76306128ee63ce964
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-03 03:24:20 +0200

    Add #groupBy

--- callbacks.js
+++ callbacks.js
@@ -284,3 +284,21 @@ console.log(
     else return "odd";
   })
 ); // should log: { odd: 3, even: 2 }
+
+/*
+Challenge 15
+Create a function groupBy that accepts an array and a callback, and returns an object. groupBy will iterate through the array and perform the callback on each element. Each return value from the callback will be saved as a key on the object. The value associated with each key will be an array consisting of all the elements that resulted in that return value when passed into the callback.
+*/
+
+function groupBy(array, callback) {
+  return array.reduce((acc, element) => {
+    (acc[callback(element)] || (acc[callback(element)] = [])).push(element);
+    return acc;
+  }, {});
+}
+
+const decimals = [1.3, 2.1, 2.4];
+const floored = function (num) {
+  return Math.floor(num);
+};
+console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }

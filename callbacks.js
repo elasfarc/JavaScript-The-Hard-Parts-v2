11d40e8c09b9f53e43156e449ab53b5402912ec8
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-04 08:32:14 +0200

    Add #rating

--- callbacks.js
+++ callbacks.js
@@ -357,3 +357,23 @@ startingObj[2] = 1;
 startingObj[12] = 4;
 const half = (n) => n / 2;
 console.log(objFilter(startingObj, half)); // should log: { 2: 1, 6: 3 }
+
+/*
+Challenge 19
+Create a function rating that accepts an array (of functions) and a value. All the functions in the array will return true or false. rating should return the percentage of functions from the array that return true when the value is used as input.
+*/
+
+// Challenge 19
+function rating(arrOfFuncs, value) {
+  return (
+    (arrOfFuncs.filter((func) => func(value)).length / arrOfFuncs.length) * 100
+  );
+}
+
+const isEven = (n) => n % 2 === 0;
+const greaterThanFour = (n) => n > 4;
+const isSquare = (n) => Math.sqrt(n) % 1 === 0;
+const hasSix = (n) => n.toString().includes("6");
+const checks = [isEven, greaterThanFour, isSquare, hasSix];
+console.log(rating(checks, 64)); // should log: 100
+console.log(rating(checks, 66)); // should log: 75

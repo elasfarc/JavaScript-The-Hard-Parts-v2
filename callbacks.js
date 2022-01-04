6b5456d2320f1dcd8264fe9cd7ec7bd7fbb4c585
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-04 08:27:37 +0200

    Add #objFilter

--- callbacks.js
+++ callbacks.js
@@ -339,3 +339,21 @@ const subtract5 = (n) => n - 5;
 console.log(commutative(multBy3, divBy4, 11)); // should log: true
 console.log(commutative(multBy3, subtract5, 10)); // should log: false
 console.log(commutative(divBy4, subtract5, 48)); // should log: false
+
+/*
+Challenge 18
+Create a function objFilter that accepts an object and a callback. objFilter should make a new object, and then iterate through the passed-in object, using each key as input for the callback. If the output from the callback is equal to the corresponding value, then that key-value pair is copied into the new object. objFilter will return this new object.
+*/
+function objFilter(obj, callback) {
+  return Object.keys(obj).reduce((filteredObj, key) => {
+    if (callback(key) === obj[key]) filteredObj[key] = obj[key];
+    return filteredObj;
+  }, {});
+}
+
+const startingObj = {};
+startingObj[6] = 3;
+startingObj[2] = 1;
+startingObj[12] = 4;
+const half = (n) => n / 2;
+console.log(objFilter(startingObj, half)); // should log: { 2: 1, 6: 3 }

100509aecd81f4e2042cc62e0ab69cfabf49567f
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-05 07:56:24 +0200

    [closure]Add #average

--- closure.js
+++ closure.js
@@ -321,3 +321,26 @@ console.log(play()); // => should log 'click'
 console.log(play()); // => should log 'bang'
 console.log(play()); // => should log 'reload to play again'
 console.log(play()); // => should log 'reload to play again'
+
+/*
+Challenge 16
+Create a function average that accepts no arguments, and returns a function (that will accept either a number as its lone argument, or no arguments at all). When the returned function is invoked with a number, the output should be average of all the numbers have ever been passed into that function (duplicate numbers count just like any other number). When the returned function is invoked with no arguments, the current average is outputted. If the returned function is invoked with no arguments before any numbers are passed in, then it should return 0.
+*/
+function average() {
+  numbers = [];
+  return (number) => {
+    if (number) numbers.push(number);
+    return numbers.length === 0
+      ? 0
+      : numbers.reduce((sum, n) => sum + n, 0) / numbers.length;
+  };
+}
+
+// /*** Uncomment these to check your work! ***/
+const avgSoFar = average();
+console.log(avgSoFar()); // => should log 0
+console.log(avgSoFar(4)); // => should log 4
+console.log(avgSoFar(8)); // => should log 6
+console.log(avgSoFar()); // => should log 6
+console.log(avgSoFar(12)); // => should log 8
+console.log(avgSoFar()); // => should log 8

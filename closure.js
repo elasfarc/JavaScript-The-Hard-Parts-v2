81654b2598aa41debc18e6f56a3c46dc7a75ece5
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-05 07:43:54 +0200

    [closure]Add #russianRoulette

--- closure.js
+++ closure.js
@@ -291,3 +291,33 @@ console.log(myNewFunc1()); // => 1
 console.log(myNewFunc1()); // => 2
 console.log(myNewFunc2()); // => 1
 console.log(myNewFunc2()); // => 2
+
+/*
+Challenge 15
+Create a function russianRoulette that accepts a number (let us call it n), and returns a function. The returned function will take no arguments, and will return the string 'click' the first n - 1 number of times it is invoked. On the very next invocation (the nth invocation), the returned function will return the string 'bang'. On every invocation after that, the returned function returns the string 'reload to play again'.
+*/
+// CHALLENGE 15
+function russianRoulette(num) {
+  const game = {
+    counter: 0,
+    onBeforeCounter: "click",
+    onCounter: "bang",
+    onAfterConter: "reload to play again",
+  };
+  return () => {
+    game.counter++;
+    return game.counter > num
+      ? game.onAfterConter
+      : game.counter < num
+      ? game.onBeforeCounter
+      : game.onCounter;
+  };
+}
+
+// /*** Uncomment these to check your work! ***/
+const play = russianRoulette(3);
+console.log(play()); // => should log 'click'
+console.log(play()); // => should log 'click'
+console.log(play()); // => should log 'bang'
+console.log(play()); // => should log 'reload to play again'
+console.log(play()); // => should log 'reload to play again'

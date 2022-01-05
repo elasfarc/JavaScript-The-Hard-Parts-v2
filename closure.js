0812137a8bf92f5e66f12570286ed0e021cecfad
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-05 07:26:13 +0200

    [closure]Add #createSecretHolder

--- closure.js
+++ closure.js
@@ -251,3 +251,25 @@ changeScene("dogs", "cats");
 changeScene("quick", "slow");
 console.log(changeScene("The quick, brown fox jumps over the lazy dogs."));
 // => should log 'The slow, brown fox jumps over the lazy cats.'
+
+/*
+Challenge 13
+There's no such thing as private properties on a JavaScript object! But, maybe there are? Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods. getSecret() which returns the secret setSecret() which sets the secret
+*/
+
+function createSecretHolder(secret) {
+  return {
+    getSecret() {
+      return secret;
+    },
+    setSecret(newSecret) {
+      secret = newSecret;
+    },
+  };
+}
+
+// /*** Uncomment these to check your work! ***/
+obj = createSecretHolder(5);
+console.log(obj.getSecret()); // => returns 5
+obj.setSecret(2);
+console.log(obj.getSecret()); // => returns 2

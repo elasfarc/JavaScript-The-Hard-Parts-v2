9e82f7e9af435020581bca45f3381bbb583441f2
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-05 03:38:28 +0200

    [closure]Add #once

--- closure.js
+++ closure.js
@@ -50,3 +50,25 @@ addByThree(2); // => should return 5
 const addByFour = addByX(4);
 addByFour(4); // => should return 8
 addByFour(5); // => should return 9
+
+/*
+Challenge 4
+Write a function once that accepts a callback as input and returns a function. When the returned function is called the first time, it should call the callback and return that output. If it is called any additional times, instead of calling the callback again it will simply return the output value from the first time it was called.
+*/
+
+// CHALLENGE 4
+function once(func) {
+  let isEverRun = false;
+  let reslut = null;
+  return (v) => {
+    if (isEverRun) return reslut;
+    isEverRun = true;
+    return (reslut = func(v));
+  };
+}
+
+// /*** Uncomment these to check your work! ***/
+const onceFunc = once(addByTwo);
+console.log(onceFunc(4)); // => should log 6
+console.log(onceFunc(10)); // => should log 6
+console.log(onceFunc(9001)); // => should log 6

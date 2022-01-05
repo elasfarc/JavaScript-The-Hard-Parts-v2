090dba1a23cacf5d8173c9b0a7f22c60aa6da48b
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-05 03:25:46 +0200

    [closure]Add #addByX

--- closure.js
+++ closure.js
@@ -27,3 +27,26 @@ const printSample = createFunctionPrinter("sample");
 printSample(); // => should console.log('sample');
 const printHello = createFunctionPrinter("hello");
 printHello(); // => should console.log('hello');
+
+/*
+Challenge 3
+create a function addByX that returns a function that will add an input by x.
+*/
+
+function addByX(x) {
+  return (n) => x + n;
+}
+
+// /*** Uncomment these to check your work! ***/
+const addByTwo = addByX(2);
+console.log(addByTwo(1)); // => should return 3
+console.log(addByTwo(2)); // => should return 4
+console.log(addByTwo(3)); // => should return 5
+
+const addByThree = addByX(3);
+addByThree(1); // => should return 4
+addByThree(2); // => should return 5
+
+const addByFour = addByX(4);
+addByFour(4); // => should return 8
+addByFour(5); // => should return 9

4263284dde97ccc7b0631371df2493b150fad7e1
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-05 04:32:40 +0200

    [closure]Add #cycleIterator

--- closure.js
+++ closure.js
@@ -157,3 +157,22 @@ const multBy2AndLog = saveOutput(multiplyBy2, "boo");
 console.log(multBy2AndLog(2)); // => should log 4
 console.log(multBy2AndLog(9)); // => should log 18
 console.log(multBy2AndLog("boo")); // => should log { 2: 4, 9: 18 }
+
+/*
+Challenge 9
+Create a function cycleIterator that accepts an array, and returns a function. The returned function will accept zero arguments. When first invoked, the returned function will return the first element of the array. When invoked a second time, the returned function will return the second element of the array, and so forth. After returning the last element of the array, the next invocation will return the first element of the array again, and continue on with the second after that, and so forth.
+*/
+
+function cycleIterator(array) {
+  let index = 0;
+  return () =>
+    index >= array.length ? ((index = 0), array[index++]) : array[index++];
+}
+
+// /*** Uncomment these to check your work! ***/
+const threeDayWeekend = ["Fri", "Sat", "Sun"];
+const getDay = cycleIterator(threeDayWeekend);
+console.log(getDay()); // => should log 'Fri'
+console.log(getDay()); // => should log 'Sat'
+console.log(getDay()); // => should log 'Sun'
+console.log(getDay()); // => should log 'Fri'

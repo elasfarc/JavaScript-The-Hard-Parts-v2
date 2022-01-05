6bac2519a893117f68cca52f0abd58201493df7e
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-05 07:18:27 +0200

    [closure]Add #censor

--- closure.js
+++ closure.js
@@ -216,3 +216,38 @@ function dateStamp(func) {
 const stampedMultBy2 = dateStamp((n) => n * 2);
 console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
 console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
+
+/*
+Challenge 12
+Create a function censor that accepts no arguments.
+censor will return a function that will accept either two strings, or one string.
+When two strings are given, the returned function will hold onto the two strings as a pair, for future use.
+When one string is given, the returned function will return the same string, except all instances of first strings (of saved pairs) will be replaced with their corresponding second strings (of those saved pairs).
+*/
+
+function censor() {
+  const pairs = {};
+  return (str1, str2) => {
+    if (str2) {
+      pairs[str1] = str2;
+      pairs[str2] = str1;
+      return;
+    }
+    return str1.split(" ").reduce((memo, word) => {
+      const regex = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+      const punctuationFree = word.replace(regex, "");
+      const punctuationMark = word.replace(punctuationFree, "");
+
+      return Object.keys(pairs).includes(punctuationFree)
+        ? `${memo} ${pairs[punctuationFree]}${punctuationMark}`
+        : `${memo} ${word}`;
+    }, "");
+  };
+}
+
+// /*** Uncomment these to check your work! ***/
+const changeScene = censor();
+changeScene("dogs", "cats");
+changeScene("quick", "slow");
+console.log(changeScene("The quick, brown fox jumps over the lazy dogs."));
+// => should log 'The slow, brown fox jumps over the lazy cats.'

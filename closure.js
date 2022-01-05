4abfc02ca2e9cb16f2f1974d48b7d9d6ec34a735
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-05 04:20:29 +0200

    [closure]Add #rollCall

--- closure.js
+++ closure.js
@@ -111,3 +111,29 @@ function delay(func, wait, ...args) {
 //   10,
 //   20
 // );
+
+/*
+Challenge 7
+Write a function rollCall that accepts an array of names and returns a function.
+ The first time the returned function is invoked, it should log the first name to the console.
+  The second time it is invoked, it should log the second name to the console, and so on, until all names have been called.
+   Once all names have been called, it should log 'Everyone accounted for'.
+*/
+
+function rollCall(names) {
+  let index = 0;
+  return () => {
+    if (index >= names.length) {
+      console.log("Everyone accounted for");
+      return;
+    }
+    console.log(names[index++]);
+  };
+}
+
+// /*** Uncomment these to check your work! ***/
+const rollCaller = rollCall(["Victoria", "Juan", "Ruth"]);
+rollCaller(); // => should log 'Victoria'
+rollCaller(); // => should log 'Juan'
+rollCaller(); // => should log 'Ruth'
+rollCaller(); // => should log 'Everyone accounted for'

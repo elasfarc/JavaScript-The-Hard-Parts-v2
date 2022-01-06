24040e16724fa5e5a72462ca0fe4d6b0b0016c3d
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-06 03:15:13 +0200

    [closure]Add #makeFuncTester

--- closure.js
+++ closure.js
@@ -344,3 +344,25 @@ console.log(avgSoFar(8)); // => should log 6
 console.log(avgSoFar()); // => should log 6
 console.log(avgSoFar(12)); // => should log 8
 console.log(avgSoFar()); // => should log 8
+
+/*
+Challenge 17
+Create a function makeFuncTester that accepts an array (of two-element sub-arrays), and returns a function (that will accept a callback). The returned function should return true if the first elements (of each sub-array) being passed into the callback all yield the corresponding second elements (of the same sub-array). Otherwise, the returned function should return false.
+*/
+function makeFuncTester(arrOfTests) {
+  return (callback) =>
+    arrOfTests
+      .map((pair) => pair[1] === callback(pair[0]))
+      .every((boolean) => boolean);
+}
+
+// /*** Uncomment these to check your work! ***/
+const capLastTestCases = [];
+capLastTestCases.push(["hello", "hellO"]);
+capLastTestCases.push(["goodbye", "goodbyE"]);
+capLastTestCases.push(["howdy", "howdY"]);
+const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
+const capLastAttempt1 = (str) => str.toUpperCase();
+const capLastAttempt2 = (str) => str.slice(0, -1) + str.slice(-1).toUpperCase();
+console.log(shouldCapitalizeLast(capLastAttempt1)); // => should log false
+console.log(shouldCapitalizeLast(capLastAttempt2)); // => should log true

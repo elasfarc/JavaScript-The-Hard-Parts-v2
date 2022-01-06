bbb2812949927fab983b3ffc5c409f3e6601c8ab
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-07 00:45:14 +0200

    [Async]Add #delayedGreet

---
+++ async.js
@@ -0,0 +1,12 @@
+/*
+Challenge 2
+Create a function delayedGreet that console logs welcome after 3 seconds.
+*/
+
+function delayedGreet() {
+  setTimeout(function () {
+    console.log("Welcome");
+  }, 3000);
+}
+
+delayedGreet();

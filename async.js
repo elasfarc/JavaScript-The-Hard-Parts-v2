40e6a965ca1c5ce7f45fe99def6abcd02af93d6d
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-07 04:15:46 +0200

    [Async]Add #SecondClock

--- async.js
+++ async.js
@@ -108,6 +108,57 @@ function promised(val) {
 }
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-const createPromise = promised("wait for it...");
-createPromise.then((val) => console.log(val));
+// const createPromise = promised("wait for it...");
+// createPromise.then((val) => console.log(val));
 // will log "wait for it..." to the console after 2 seconds
+
+/*
+Challenge 9
+Write a SecondClock class, with two methods: start and reset.​
+start: upon invocation, invokes a callback (this.cb, defined in the constructor) on an argument every second, with the argument to the callback being the current seconds "value".
+
+In other words, the callback gets invoked every second on the "seconds hand" of the clock. Always start with 1 and don't utilize the seconds value of the current computer clock time.
+
+The first "tick" with value 1 occurs 1 second after the initial "secondClock" invocation.
+The second "tick" with value 2 occurs 2 seconds after the initial "secondClock" invocation.
+...
+The sixtieth "tick" with value 60 occurs 60 seconds after the initial "secondClock" invocation.
+The sixty-first "tick" with value 1 occurs 61 seconds after the initial "secondClock" invocation.
+The sixty-second "tick" with value 2 occurs 62 seconds after the initial "secondClock" invocation.
+etc.
+reset: upon invocation, completely stops the "clock".
+Also resets the time back to the beginning.
+​
+Hint: look up setInterval and clearInterval.
+
+
+*/
+
+class SecondClock {
+  constructor(cb) {
+    this.cb = cb;
+    this.seconds = 0;
+  }
+  start() {
+    this.id = setInterval(() => {
+      //this.seconds = this.seconds > 60 ? 1 : ++this.seconds;
+      this.seconds++;
+      this.cb(this.seconds % 60);
+    }, 1000);
+  }
+  reset() {
+    this.seconds = 0;
+    clearInterval(this.id);
+  }
+}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+const clock = new SecondClock((val) => {
+  console.log(val);
+});
+console.log("Started Clock.");
+clock.start();
+setTimeout(() => {
+  clock.reset();
+  console.log("Stopped Clock after 6 seconds.");
+}, 6000);

02504337cc5b951e5004adac01a7d6daf3741260
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-07 02:11:39 +0200

    [Async]Add #delayCounter

--- async.js
+++ async.js
@@ -68,4 +68,28 @@ function everyXsecsForYsecs(func, interval, duration) {
 function theEnd() {
   console.log("This is the end!");
 }
-everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+// everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+
+/*
+Challenge 7
+Write a function delayCounter that accepts a number (called 'target') as the first argument and a number of milliseconds (called 'wait') as the second argument, and returns a function.
+
+When the returned function is invoked, it should log to the console all of the numbers between 1 and the target number, spaced apart by 'wait' milliseconds.
+*/
+
+function delayCounter(target, wait) {
+  return () => {
+    let counter = 0;
+    const id = setInterval(() => {
+      console.log(++counter);
+      if (counter >= target) clearInterval(id);
+    }, wait);
+  };
+}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+const countLogger = delayCounter(3, 1000);
+countLogger();
+// After 1 second, log 1
+// After 2 seconds, log 2
+// After 3 seconds, log 3

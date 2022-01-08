4843694ccce25d131ee53eba71a9f688815583bd
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-08 05:57:53 +0200

    [Promises]Add #sayHello

---
+++ promises.js
@@ -0,0 +1,8 @@
+/*
+Challenge 1
+Let's start by reviewing asynchronous functions! Using setTimeout, print the string 'Hello!' after 1000ms.
+*/
+
+const sayHello = () => setTimeout(() => console.log("Hello"), 1000);
+
+// sayHello();

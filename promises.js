c9ebb8fb0476402a9357b609b5ca3667a1033365
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-08 06:01:06 +0200

    [Promises]Add resolved-promise

--- promises.js
+++ promises.js
@@ -6,3 +6,14 @@ Let's start by reviewing asynchronous functions! Using setTimeout, print the str
 const sayHello = () => setTimeout(() => console.log("Hello"), 1000);
 
 // sayHello();
+
+/*
+Challenge 2
+Create a promise. Have it resolve with a value of 'Resolved!' in resolve after a delay of 1000ms, using setTimeout. Print the contents of the promise after it has been resolved by passing console.log to .then
+*/
+
+const promise = new Promise((resolve) =>
+  setTimeout(() => resolve("Resolved"), 1000)
+);
+
+promise.then((v) => console.log(v));

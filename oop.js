abca83ce94badead014df26dc4d9efc3f894d1d5
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-09 16:06:35 +0200

    [OOP] MIXINS

--- oop.js
+++ oop.js
@@ -199,3 +199,35 @@ const adminFromFactory = adminFactory("Eva", 5);
 // /********* Uncomment these lines to test your work! *********/
 adminFromFactory.sayType(); // -> Logs "I am a Admin"
 adminFromFactory.sharePublicMessage(); // -> Logs "Welcome users!"
+
+/*
+EXTENSION: Mixins
+Mixins are a tool in object-oriented programming that allows objects to be given methods and properties beyond those provided by their inheritance. For this challenge, complete the missing code, giving all of the robotMixin properties to robotFido. Do this in a single line of code, without adding the properties individually.
+*/
+
+/****************************************************************
+EXTENSION: MIXINS
+****************************************************************/
+
+class Dog {
+  constructor() {
+    this.legs = 4;
+  }
+  speak() {
+    console.log("Woof!");
+  }
+}
+
+const robotMixin = {
+  skin: "metal",
+  speak: function () {
+    console.log(`I have ${this.legs} legs and am made of ${this.skin}`);
+  },
+};
+
+let robotFido = new Dog();
+
+robotFido = Object.assign(robotFido, robotMixin);
+
+// /********* Uncomment to test your work! *********/
+robotFido.speak(); // -> Logs "I am made of metal"

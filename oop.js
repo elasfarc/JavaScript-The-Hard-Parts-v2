30a8a100fc2fc271aa62a5c95f8548aa85894b6c
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-09 15:18:57 +0200

    [OOP] challenge 5, 6 and 7

--- oop.js
+++ oop.js
@@ -62,3 +62,45 @@ console.log(sandra.age); //-> Logs 26
 sandra.greet(); //-> Logs 'hello'
 
 sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
+
+/*
+/****************************************************************
+                    USING THE 'NEW' KEYWORD
+****************************************************************
+Using the NEW keyword
+Challenge 5
+Create a function PersonConstructor that uses the this keyword to save a single property onto its scope called greet. greet should be a function that logs the string 'hello'.
+Challenge 6
+Create a function personFromConstructor that takes as input a name and an age. When called, the function will create person objects using the new keyword instead of the Object.create method.
+Challenge 7
+Without editing the code you've already written, add an introduce method to the PersonConstructor function that logs "Hi, my name is [name]".
+
+*/
+
+function PersonConstructor() {
+  this.greet = function () {
+    console.log("hello");
+  };
+}
+
+function personFromConstructor(name, age) {
+  const person = new PersonConstructor();
+  person.name = name;
+  person.age = age;
+  return person;
+}
+
+PersonConstructor.prototype.introduce = function () {
+  console.log("Hi, my name is " + this.name);
+};
+
+const simon = new PersonConstructor();
+simon.greet(); // -> Logs 'hello'
+const mike = personFromConstructor("Mike", 30);
+
+// /********* Uncomment these lines to test your work! *********/
+console.log(mike.name); // -> Logs 'Mike'
+console.log(mike.age); //-> Logs 30
+mike.greet(); //-> Logs 'hello'
+
+mike.introduce(); // -> Logs 'Hi, my name is Mike'

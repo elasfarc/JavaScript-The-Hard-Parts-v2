8c976161bc83abbc3d5ff65049bc82615983ebe3
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-09 15:23:30 +0200

    [OOP] challenge 8 and 9

--- oop.js
+++ oop.js
@@ -94,13 +94,45 @@ PersonConstructor.prototype.introduce = function () {
   console.log("Hi, my name is " + this.name);
 };
 
+// /********* Uncomment these lines to test your work! *********/
 const simon = new PersonConstructor();
 simon.greet(); // -> Logs 'hello'
 const mike = personFromConstructor("Mike", 30);
 
-// /********* Uncomment these lines to test your work! *********/
 console.log(mike.name); // -> Logs 'Mike'
 console.log(mike.age); //-> Logs 30
 mike.greet(); //-> Logs 'hello'
 
 mike.introduce(); // -> Logs 'Hi, my name is Mike'
+
+/*
+Using ES6 Classes
+Challenge 8
+Create a class PersonClass. PersonClass should have a constructor that is passed an input of name and saves it to a property by the same name. PersonClass should also have a method called greet that logs the string 'hello'.
+Challenge 9
+Create a class DeveloperClass that creates objects by extending the PersonClass class. In addition to having a name property and greet method, DeveloperClass should have an introduce method. When called, introduce should log the string 'Hello World, my name is [name]'.
+
+*/
+
+class PersonClass {
+  constructor(name) {
+    this.name = name;
+  }
+  greet() {
+    console.log("hello");
+  }
+}
+
+class DeveloperClass extends PersonClass {
+  introduce() {
+    console.log("Hello World, my name is " + this.name);
+  }
+}
+
+// /********* Uncomment these lines to test your work! *********/
+const george = new PersonClass();
+george.greet(); // -> Logs 'hello'
+
+const thai = new DeveloperClass("Thai", 32);
+console.log(thai.name); // -> Logs 'Thai'
+thai.introduce(); //-> Logs 'Hello World, my name is Thai'
